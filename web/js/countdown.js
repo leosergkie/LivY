19ccb189acf01aaac1f83b136b3b494c85df6f44
Vlad <Nakmak1998@gmail.com>
2018-07-09 22:44:15 +0300

    Готова крутая форма

--- web/js/countdown.js
+++ web/js/countdown.js
@@ -1,6 +1,33 @@
+﻿
+// Таймер
 $('#timer').countdown('2018/07/10').on('update.countdown', function(event) {
   var $this = $(this).html(event.strftime(''
     + '<div class = "timer_number">%I<br><span>ЧАСОВ</span></div> : '
     + '<div class = "timer_number">%M<br><span>МИНУТ</span></div> : '
     + '<div class = "timer_number">%S<br><span>СЕКУНД</span></div>'));
 });
+
+//Отправка формы ajax-запросом
+$('form').submit(function(e){
+    e.preventDefault();
+    $.ajax({
+      url: "http://localhost/web/sendOrderForm.php",
+      type: "POST",
+      data: $('#form').serialize(),
+      success: function(response) {
+        $('.order_info').empty();
+        $('.order_info').append(function() {
+         return "<p>Спасибо!</p><p>С вами свяжутся в ближайшее время.</p><br><br>"
+        });
+         $('#form')[0].reset();
+      },
+      error: function(response) {
+        //обработка ошибок при отправке
+     }
+    });
+});
+
+//Маска телефонного номера
+ jQuery(function($){
+   $("#phone_icon").mask("+7(999) 999-99-99");
+   });
---
+++ web/sendOrderForm.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+if (isset($_POST['Name'])){
+        $name = $_POST['Name'];
+        $contacts = $_POST['Phone'];
+        $to='user@example.com';
+        $subject='=?utf-8?B?'.base64_encode('Тест').'?=';
+        $message=chunk_split(base64_encode("Имя: $name Телефон: $contacts"));
+        $headers='MIME-Version: 1.0'."\r\n".
+                 'Content-Transfer-Encoding: base64'."\r\n".
+                 'Content-Type: text/plain; charset=utf-8';
+        mail($to,$subject,$message,$headers);
+        } 
+ ?>
